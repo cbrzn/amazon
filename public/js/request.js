6fc845e755b11884d7cf91c962cc7dd56af6347e
cbrzn <cesarbrazon10@gmail.com>
2018-01-23 01:16:18 -0400

    testing heroku

--- public/js/request.js
+++ public/js/request.js
@@ -13,15 +13,4 @@ function session(){
     });
 };
 
-function new_account(){
-    let username = $('username').value;
-    let email = $('email').value;
-    let password = $('password').value;
-    xhr.post(`../signup`,{username:username, email:email, password:password},{'Content-Type':'application/json'}).then((data)=>{
-      console.log(data);
-      if (data.status == 200) {
-        alert("You have registered! Now log in");
-        window.location.href = "./login.html";
-      }
-    });
-};
+$('login').addEventListener('click', session);
---
+++ public/js/signup.js
@@ -0,0 +1,20 @@
+let xhr = new XHR();
+function $(id) {
+    return document.getElementById(id);
+};
+
+
+function new_account(){
+    let username = $('username').value;
+    let email = $('email').value;
+    let password = $('password').value;
+    xhr.post(`../signup`,{username:username, email:email, password:password},{'Content-Type':'application/json'}).then((data)=>{
+      console.log(data);
+      if (data.status == 200) {
+        alert("You have registered! Now log in");
+        window.location.href = "./login.html";
+      }
+    });
+};
+
+$('signup').addEventListener('click', new_account);
