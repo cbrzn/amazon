f01668ae20fa6c3c52ae7fe2bdd608c8cff6948b
cbrzn <cesarbrazon10@gmail.com>
2018-01-23 01:28:08 -0400

    testing heroku

--- public/js/request.js
+++ public/js/request.js
@@ -6,7 +6,7 @@ function $(id) {
 function session(){
     let username = $('email').value;
     let password = $('password').value;
-    xhr.post(`../login`,{username:username,password:password},{'Content-Type':'application/json'}).then((data)=>{
+    xhr.post(`./login`,{username:username,password:password},{'Content-Type':'application/json'}).then((data)=>{
       console.log(data);
         if (data.status != 400)
             window.location.href = "./index.html";
