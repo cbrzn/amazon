a6af352215be30d9a170838e2fec19bc6d117ab4
cbrzn <cesarbrazon10@gmail.com>
2018-01-25 17:01:10 -0400

    uploading to cloud

--- controllers/upload.js
+++ controllers/upload.js
@@ -10,15 +10,15 @@ cloudinary.config({
 });
 
 
-  let storage = multer.diskStorage({
+/*  let storage = multer.diskStorage({
     destination: function (req, file, cb) {
       cb(null, "./uploads/");
     },
     filename: function (req, file, cb) {
       cb(null, `${file.originalname}`)
        }
-    })
-let upload = multer({storage: storage});
+    })*/
+let upload = multer({dest: "./uploads/")});
 
 
 let router = express.Router();
